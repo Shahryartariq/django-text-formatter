3341b53469adc32da24f760bd1cabce88bda6a99
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-02-22 22:07:32 +0500

    added the project pipeline

--- text_format_django/views.py
+++ text_format_django/views.py
@@ -1,8 +1,25 @@
 from django.http import HttpResponse
 
-def index (request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+
+def index(request):
+    return HttpResponse("<h1>HOME</h1>")
 
 
-def about (request):
-    return HttpResponse("About Hello, World!")
+def removepunc(request):
+    return HttpResponse("<h1>Removed Punctuation</h1>")
+
+
+def capfirst(request):
+    return HttpResponse("<h1>Capitalized First Letter</h1>")
+
+
+def newlineremove(request):
+    return HttpResponse("<h1>New Line Removed</h1>")
+
+
+def spaceremove(request):
+    return HttpResponse("<h1>Extra Spaces Removed</h1>")
+
+
+def charcount(request):
+    return HttpResponse("<h1>Character Counted</h1>")

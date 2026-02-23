2e7207d2460a01de125700e13bdc1cd5e8d52798
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-02-24 00:35:04 +0500

    Added Template settings

--- text_format_django/views.py
+++ text_format_django/views.py
@@ -1,25 +1,26 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>HOME</h1>")
+    params = {'name': 'Sherry', 'place': 'Earth'}
+    return render(request, 'index.html', params)
 
 
 def removepunc(request):
-    return HttpResponse("<h1>Removed Punctuation</h1>")
+    return HttpResponse("<h1>Removed Punctuation</h1> <a href='/'>Back</a>")
 
 
 def capfirst(request):
-    return HttpResponse("<h1>Capitalized First Letter</h1>")
+    return HttpResponse("<h1>Capitalized First Letter</h1> <a href='/'>Back</a>")
 
 
 def newlineremove(request):
-    return HttpResponse("<h1>New Line Removed</h1>")
+    return HttpResponse("<h1>New Line Removed</h1> <a href='/'>Back</a>")
 
 
 def spaceremove(request):
-    return HttpResponse("<h1>Extra Spaces Removed</h1>")
+    return HttpResponse("<h1>Extra Spaces Removed</h1> <a href='/'>Back</a>")
 
 
 def charcount(request):
-    return HttpResponse("<h1>Character Counted</h1>")
+    return HttpResponse("<h1>Character Counted</h1> <a href='/'>Back</a>")

4d6b145d1f0fc20525fb7560bfb64ec38309e187
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-02-27 00:33:31 +0500

    Added About

--- text_format_django/views.py
+++ text_format_django/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
 
 def analyze(request):
 
